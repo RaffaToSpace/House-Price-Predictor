4edeb701a93e8afb444668cb5889c17f2f93ec45
RaffaToSpace <lordbeff@gmail.com>
2020-04-29 23:41:40 +0100

    Data prep package: inclusion of the necessary files, version 0.1 uploaded on test Pypi

--- pthp_prep_package/setup.py
+++ pthp_prep_package/setup.py
@@ -13,5 +13,5 @@ setup(name='pthp_data_prep',
       description='Data preparation and feature engineering for the house price predictor of the dataset in references.',
       packages=['pthp_data_prep'],
       author='Raffaele Nolli',
-      author_email='raffaele.nolli@outlook.com'
+      author_email='raffaele.nolli@outlook.com',
       zip_safe=False)
